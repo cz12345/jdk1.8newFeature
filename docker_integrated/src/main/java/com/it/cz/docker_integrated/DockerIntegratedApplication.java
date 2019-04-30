15aaeac25b5c0d18553e4cb3a64a644871cfef0d
zchen25 <zchen25@its.jnj.com>
2019-04-30 22:05:21 +0800

    完成docker-maven插件实现镜像自动部署和推送到私服

---
+++ docker_integrated/src/main/java/com/it/cz/docker_integrated/DockerIntegratedApplication.java
@@ -0,0 +1,13 @@
+package com.it.cz.docker_integrated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntegratedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerIntegratedApplication.class, args);
+    }
+
+}
---
+++ docker_integrated/src/test/java/com/it/cz/docker_integrated/DockerIntegratedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.it.cz.docker_integrated;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerIntegratedApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
